62a8a9c24d6bc9f48c1ff7bc6ca69f1ca0d5fd83
tsuguri <tsuguri4429@gmail.com>
2023-03-04 15:26:19 +0900

    first commit

---
+++ VBTwatch Watch App/ActivityClassifier.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  VBTwatch Watch App
+//
+//  Created by Ryo Yoshitsugu on R 5/03/04.
+//
+
+import Foundation
---
+++ VBTwatch Watch App/ExtendedRunTime.swift
@@ -0,0 +1,8 @@
+//
+//  ExtendedRunTime.swift
+//  VBTwatch Watch App
+//
+//  Created by Ryo Yoshitsugu on R 5/03/04.
+//
+
+import Foundation
