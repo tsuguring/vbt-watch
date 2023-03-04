827d1fb69454faded2db497fc6212bb5752f3702
tsuguri <tsuguri4429@gmail.com>
2023-03-04 09:39:04 +0900

    Initial Commit

---
+++ VBTwatch Watch App/VBTwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  VBTwatchApp.swift
+//  VBTwatch Watch App
+//
+//  Created by Ryo Yoshitsugu on R 5/03/04.
+//
+
+import SwiftUI
+
+@main
+struct VBTwatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VBTwatch/VBTwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  VBTwatchApp.swift
+//  VBTwatch
+//
+//  Created by Ryo Yoshitsugu on R 5/03/04.
+//
+
+import SwiftUI
+
+@main
+struct VBTwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
