1a9a68f07bc572be87c80900b470d0e9ee0d9c79
tsuguri <tsuguri4429@gmail.com>
2023-03-05 16:04:57 +0900

    measure velocity per benchpress

---
+++ VBTwatch Watch App/VelocityMeasurement.swift
@@ -0,0 +1,34 @@
+//
+//  VelocityMeasurement.swift
+//  VBTwatch Watch App
+//
+//  Created by Ryo Yoshitsugu on R 5/03/05.
+//
+
+import Foundation
+import CoreMotion
+
+class VelocityMeasurement {
+    static let G_ACCEL = 9.81
+    var timeSpan = 0.0
+    var velocity = 0.0
+    private var lastAccel = 0.0
+    
+    init(timeSpan: Double) {
+        self.timeSpan = timeSpan
+    }
+    
+    func calculateVelocity(accel: Double) {
+        velocity += (lastAccel + calculateMeterPerSecondSquared(accel: accel)) * timeSpan / 2
+        lastAccel = calculateMeterPerSecondSquared(accel: accel)
+    }
+    
+    func calculateMeterPerSecondSquared(accel: Double) -> Double{
+        return abs(accel * VelocityMeasurement.G_ACCEL)
+    }
+    
+    func initializeVelocityData() {
+        velocity = 0.0
+        lastAccel = 0.0
+    }
+}
